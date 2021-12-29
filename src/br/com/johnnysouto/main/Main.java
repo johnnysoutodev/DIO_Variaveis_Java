974976fad54fdb7239e3853be401984e91238297
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-12-29 19:07:22 -0300

    testando varaveis

--- src/br/com/johnnysouto/main/Main.java
+++ src/br/com/johnnysouto/main/Main.java
@@ -3,5 +3,12 @@ package br.com.johnnysouto.main;
 public class Main {
     public static void main(String[] args) {
 
+        int i;
+        int 1;
+        int _1a;
+        int $aq = 7;
+
+        final int j = 18;
+//        j = 22;
     }
 }
