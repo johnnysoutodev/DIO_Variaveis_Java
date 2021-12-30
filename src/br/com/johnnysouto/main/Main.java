7aacfc37d6a5a664b9a24c4f11b69f9ac16f32c4
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-12-29 22:03:42 -0300

    impressão na tela das variaveis criadas.

--- src/br/com/johnnysouto/main/Main.java
+++ src/br/com/johnnysouto/main/Main.java
@@ -15,5 +15,52 @@ public class Main {
         long l1 = 1000000000000000000L;
         long l2 = 2004005000500055000L;
 
+        float f1 = 4.5f;
+        float f2 = 10.68F;
+
+        double d1 = 85.69;
+        double d2 = 99.84D;
+
+        char c1 = 'W';
+        char c2 = 'T';
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b1);
+
+        System.out.println(s1);
+        System.out.println(s1);
+
+        System.out.println(l1);
+        System.out.println(l2);
+
+        System.out.println(i1);
+        System.out.println(i2);
+
+        System.out.println(f1);
+        System.out.println(f2);
+
+        System.out.println(f1);
+        System.out.println(f2);
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c1);
+
+        System.out.println(st1);
+        System.out.println(st2);
+
+        System.out.println(bo1);
+        System.out.println(bo2);
+
     }
 }
