237a98f3629844ff7428b9e4d31b847d394958db
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-12-29 21:50:34 -0300

    add as variaveis pedido no enunciado

--- src/br/com/johnnysouto/main/Main.java
+++ src/br/com/johnnysouto/main/Main.java
@@ -3,6 +3,17 @@ package br.com.johnnysouto.main;
 public class Main {
     public static void main(String[] args) {
 
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        short s2 = 4000;
+
+        int i1 = 1000000000;
+        int i2 = 285000;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 2004005000500055000L;
 
     }
 }
